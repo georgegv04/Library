93a26c59694593572c3e72dd59f6e5b28f9c47a9
Georgi Vasilev <georgegv04@gmail.com>
2026-06-17 19:16:16 +0300

    Display first book title

--- script.js
+++ script.js
@@ -14,4 +14,8 @@ const library = {
 library.addBook("Ο Άγιος Παϊσιος ο Αγιορείτης", "Άγιος Παϊσιος", 665, true);
 library.addBook("Βίος και Λόγοι", "Άγιος Πορφύριος", 600, true);
 library.addBook("The Brothers Karamazov", "Fyodor Dostoevsky", 885, false);
-console.log(library.booksList);
+// console.log(library.booksList);
+
+const firstBookTitle = document.querySelector(".first-book-title");
+
+firstBookTitle.textContent = library.booksList[0].title;
