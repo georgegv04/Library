02822a9fe24bc61a92913912007a88cb9f0d2104
Georgi Vasilev <georgegv04@gmail.com>
2026-06-17 18:52:35 +0300

    Add basic library data structure

---
+++ script.js
@@ -0,0 +1,17 @@
+const library = {
+  booksList: [],
+
+  addBook(title, author, pages, readStatus) {
+    this.booksList.push({
+      title,
+      author,
+      pages,
+      isRead: readStatus,
+    });
+  },
+};
+
+library.addBook("Ο Άγιος Παϊσιος ο Αγιορείτης", "Άγιος Παϊσιος", 665, true);
+library.addBook("Βίος και Λόγοι", "Άγιος Πορφύριος", 600, true);
+library.addBook("The Brothers Karamazov", "Fyodor Dostoevsky", 885, false);
+console.log(library.booksList);
