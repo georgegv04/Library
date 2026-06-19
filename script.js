df8d96d166433636abceed2130967d5567e9a122
Georgi Vasilev <georgegv04@gmail.com>
2026-06-19 10:34:35 +0300

    Books are stored in data and displayed on the page

--- script.js
+++ script.js
@@ -14,12 +14,20 @@ const library = {
 library.addBook("Ο Άγιος Παϊσιος ο Αγιορείτης", "Άγιος Παϊσιος", 665, true);
 library.addBook("Βίος και Λόγοι", "Άγιος Πορφύριος", 600, true);
 library.addBook("The Brothers Karamazov", "Fyodor Dostoevsky", 885, false);
-// console.log(library.booksList);
 
 const libraryContainer = document.querySelector(".library-container");
 
 for (let i = 0; i < library.booksList.length; i++) {
   let bookCard = document.createElement("div");
   bookCard.classList.add("book-card");
+
+  let bookDetails = document.createElement("div");
+  bookDetails.textContent =
+    library.booksList[i].title +
+    library.booksList[i].author +
+    library.booksList[i].pages +
+    library.booksList[i].isRead;
+  bookCard.appendChild(bookDetails);
+
   libraryContainer.appendChild(bookCard);
 }
