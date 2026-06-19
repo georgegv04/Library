83d8198d75cde0c9a85e24574500fc200349e452
Georgi Vasilev <georgegv04@gmail.com>
2026-06-19 13:33:57 +0300

    Separate book card content elements

--- script.js
+++ script.js
@@ -11,9 +11,14 @@ const library = {
   },
 };
 
-library.addBook("Ο Άγιος Παϊσιος ο Αγιορείτης", "Άγιος Παϊσιος", 665, true);
-library.addBook("Βίος και Λόγοι", "Άγιος Πορφύριος", 600, true);
-library.addBook("The Brothers Karamazov", "Fyodor Dostoevsky", 885, false);
+library.addBook("Ο Άγιος Παϊσιος ο Αγιορείτης", "Άγιος Παϊσιος", 665, "Read");
+library.addBook("Βίος και Λόγοι", "Άγιος Πορφύριος", 600, "Read");
+library.addBook(
+  "The Brothers Karamazov",
+  "Fyodor Dostoevsky",
+  885,
+  "Not read yet",
+);
 
 const libraryContainer = document.querySelector(".library-container");
 
@@ -21,14 +26,25 @@ for (let i = 0; i < library.booksList.length; i++) {
   let bookCard = document.createElement("div");
   bookCard.classList.add("book-card");
 
-  let bookDetails = document.createElement("div");
-  bookDetails.classList.add("book-details");
-  bookDetails.textContent =
-    library.booksList[i].title +
-    library.booksList[i].author +
-    library.booksList[i].pages +
-    library.booksList[i].isRead;
-  bookCard.appendChild(bookDetails);
+  let bookTitle = document.createElement("div");
+  bookTitle.classList.add("book-title");
+  bookTitle.textContent = library.booksList[i].title;
+  bookCard.appendChild(bookTitle);
+
+  let bookAuthor = document.createElement("div");
+  bookAuthor.classList.add("book-author");
+  bookAuthor.textContent = library.booksList[i].author;
+  bookCard.appendChild(bookAuthor);
+
+  let bookPages = document.createElement("div");
+  bookPages.classList.add("book-pages");
+  bookPages.textContent = library.booksList[i].pages;
+  bookCard.appendChild(bookPages);
+
+  let bookStatus = document.createElement("div");
+  bookStatus.classList.add("book-status");
+  bookStatus.textContent = library.booksList[i].isRead;
+  bookCard.appendChild(bookStatus);
 
   libraryContainer.appendChild(bookCard);
 }
